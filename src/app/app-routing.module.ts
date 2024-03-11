839258d047de2e26c4815c53f1a5ab37bc4b7901
ivana4app <ivan.anic@4app-solution.hr>
2024-03-11 23:57:28 +0100

    sitnice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -147,7 +147,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true,
+    useHash: false,
     scrollPositionRestoration: 'disabled',
   })],
   exports: [RouterModule]
