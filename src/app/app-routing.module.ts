b244b95ac6159174528f73ebd4ef6eb6f421d4be
Matija <95matija@gmail.com>
2024-06-30 13:26:57 +0200

    promjenev2 treba promjenit hidden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -153,7 +153,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true,
+    useHash: false,
     scrollPositionRestoration: 'disabled',
     initialNavigation: 'enabledBlocking'
   })],
